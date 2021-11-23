df83220ab8f82e0edabcde53d1469108a15b8424
emmanuel2612 <ojijimanuel@hotmail.com>
2021-11-23 05:19:21 +0000

    Changed 'Yours' to 'Today'

--- src/components/Home.js
+++ src/components/Home.js
@@ -6,7 +6,7 @@ const Home = (props) => {
   return (
     <section className="Home">
       <div className="landscape"></div>
-      <h2 className="section-title">Yours</h2>
+      <h2 className="section-title">Today</h2>
       <div class="quote-verse-wrap">
         <Quote backgroundColor="#42ccc9" />
         <Verse backgroundColor="#4fa7d3" />

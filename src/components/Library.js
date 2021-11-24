88bf1865897d10c287b93854ff01cb2863a12ac7
emmanuel2612 <ojijimanuel@hotmail.com>
2021-11-24 00:21:11 +0000

    Added Sound cards

--- src/components/Library.js
+++ src/components/Library.js
@@ -2,15 +2,29 @@ import Quote from "./Affirmation";
 import Verse from "./Verse";
 import "./Home.scss";
 
-import pageBillboard from '../morning-landscape.jpg'
+import pageBillboard from "../morning-landscape.jpg";
+import SoundCard from "./SoundCard";
 
 const Library = (props) => {
   return (
     <>
-      <div className="page-billboard" style={{backgroundImage: `url(${pageBillboard})`}}></div>
-      <h2 className="section-title">Library</h2>
-      <h3>SOUNDS</h3>
-  </>
+      <div
+        className="page-billboard"
+        style={{ backgroundImage: `url(${pageBillboard})` }}
+      ></div>
+      <h2 className="section-heading">Library</h2>
+      <h3 className="section-sub-heading">SOUNDS</h3>
+      <div className="sound-row">
+        <SoundCard />
+        <SoundCard />
+        <SoundCard />
+        <SoundCard />
+        <SoundCard />
+        <SoundCard />
+        <SoundCard />
+        <SoundCard />
+      </div>
+    </>
   );
 };
 
---
+++ src/components/SoundCard.js
@@ -0,0 +1,12 @@
+import './SoundCard.scss'
+
+const SoundCard = () => {
+    return(
+        <div className="SoundCard">
+            <img src="https://via.placeholder.com/70"/>
+            <span>Rain</span>
+        </div>
+    )
+}
+
+export default SoundCard;

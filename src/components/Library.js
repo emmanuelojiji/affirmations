d1e6366777f8bdd63ee3806f333f176c809b052b
emmanuel2612 <ojijimanuel@hotmail.com>
2021-11-25 21:22:54 +0000

    Vector background soundcard added

--- src/components/Library.js
+++ src/components/Library.js
@@ -5,10 +5,10 @@ import "./Library.scss";
 import pageBillboard from "../morning-landscape.jpg";
 import SoundCard from "./SoundCard";
 
-import OceanIcon from '../ocean-icon.webp'
-import RainIcon from '../rain-icon.jpg'
-import FireplaceIcon from '../fireplace-icon.jpg'
-import WaterfallIcon from '../waterfall-icon.jpg'
+import OceanIcon from "../ocean-icon.PNG";
+import RainIcon from "../rain-icon.PNG";
+import FireplaceIcon from "../fireplace-icon.jpg";
+import WaterfallIcon from "../waterfall-icon.jpg";
 
 const Library = () => {
   return (
@@ -20,14 +20,14 @@ const Library = () => {
       <h2 className="section-heading">Library</h2>
 
       <div className="sub-section-row">
-      <h3 className="section-sub-heading">SOUNDS</h3>
-      <div className="sound-row">
-        <SoundCard soundTitle="Rain" backgroundImage={RainIcon}/>
-        <SoundCard soundTitle="Ocean" backgroundImage={OceanIcon}/>
-        <SoundCard soundTitle="Fireplace" backgroundImage={FireplaceIcon}/>
-        <SoundCard soundTitle="Waterfall"  backgroundImage={WaterfallIcon}/>
-        <SoundCard soundTitle="Title" />
-        <SoundCard soundTitle="Title" />
+        <h3 className="section-sub-heading">SOUNDS</h3>
+        <div className="sound-row">
+          <SoundCard soundTitle="Rain" backgroundImage={RainIcon} />
+          <SoundCard soundTitle="Ocean" backgroundImage={OceanIcon} />
+          <SoundCard soundTitle="Fireplace" backgroundImage={FireplaceIcon} />
+          <SoundCard soundTitle="Sound" />
+          <SoundCard soundTitle="Sound" />
+          <SoundCard soundTitle="Sound" />
         </div>
       </div>
 
